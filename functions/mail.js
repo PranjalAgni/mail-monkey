61967f17f1c96ca7db27c8e6dac7ede353ba33d8
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-03-18 12:24:46 +0530

    :tada: added cloud function + mailgun service to send mails

--- functions/mail.js
+++ functions/mail.js
@@ -1,7 +1,35 @@
+require('dotenv').config();
 exports.handler = (event, _context, callback) => {
-  console.log(event.body);
-  callback(null, {
-    statusCode: 500,
-    body: JSON.stringify({ yoyo: 'wowowoow' })
+  const mailgun = require('mailgun-js');
+  const mg = new mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
   });
+
+  const data = JSON.parse(event.body);
+
+  console.log(data);
+
+  const email = {
+    from: `Pranjal Agnihotri <beingPranjal6@gmail.com>`,
+    to: `${data.name} <${data.email}>`,
+    subject: data.subject,
+    body: data.body
+  };
+
+  mg.messages().send(email, (error, response) => {
+    if (error) console.error('ERROR::::', error);
+    else console.log('RESULT::::', response);
+    callback(error, {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    });
+  });
+
+  // callback(null, {
+  //   statusCode: 200,
+  //   body: JSON.stringify({
+  //     yoyo: 'HEYHEY'
+  //   })
+  // });
 };
